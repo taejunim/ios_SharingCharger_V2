cebf3e86ff1452724ad0727ced3b88e12321e8fb
joyouyeong <dbdud2407@gmail.com>
2021-04-14 14:36:06 +0900

    1. 사이드 메뉴 2. 임시로 버튼 만들기 (정리해야됨)

---
+++ SharingCharger_V2/ViewController/MainStoryBoardViewController/LeftMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LeftMenuViewController.swift
+//  SharingCharger_V2
+//  Description - 좌측 메뉴 ViewController
+//  Created by 조유영 on 2021/04/14.
+//
+
+import Alamofire
+
+class LeftMenuViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
--- SharingCharger_V2/ViewController/MainStoryBoardViewController/MainViewController.swift
+++ SharingCharger_V2/ViewController/MainStoryBoardViewController/MainViewController.swift
@@ -6,11 +6,14 @@
 //
 import UIKit
 import CoreLocation
+import SideMenu
 
 class MainViewController: UIViewController, MTMapViewDelegate {
 
     @IBOutlet weak var mapView: UIView!
     var mTMapView: MTMapView?
+    
+    let addressView = ShadowButton(type: .system)
     
     let locationManager = CLLocationManager()
     
@@ -28,6 +31,12 @@ class MainViewController: UIViewController, MTMapViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         super.viewWillAppear(animated)
+        
+        //위치 권한이 없으면 위치 권한 메시지 띄움
+        if !hasLocationPermission() {
+            print("권한없음")
+            self.locationManager.requestAlwaysAuthorization()
+        }
     }
     
     //view가 나타난 후
@@ -35,15 +44,91 @@ class MainViewController: UIViewController, MTMapViewDelegate {
         print("MainViewController - viewDidAppear")
         super.viewDidAppear(animated)
         
-        print("롸???    \(locationManager.location?.coordinate)")
         if let defaultLatitude = locationManager.location?.coordinate.latitude , let defaultLongitude = locationManager.location?.coordinate.longitude{
-                
-            print("왜 안탐")
+            // 현재 위치 좌표를 받아서 맵뷰 가운데에 맞춰줌
             let DEFAULT_POSITION = MTMapPointGeo(latitude: defaultLatitude, longitude: defaultLongitude)
             mTMapView?.setMapCenter(MTMapPoint(geoCoord: DEFAULT_POSITION), zoomLevel: 1, animated: true)
         }
     }
+
+    private func viewWillInitializeObjects(){
+        
+
+        
+        //지도 설정
+        mTMapView = MTMapView(frame: mapView.bounds)
+        if let mTMapView = mTMapView {
+            mTMapView.delegate = self
+            mTMapView.baseMapType = .standard
+            mapView.addSubview(mTMapView)
+        }
+        //메인 화면 버튼 추가
+        addButton(buttonName: "menu", width: 40, height: 40, top: 15, left: 15, right: nil, bottom: nil, target: mapView)   //사이드 메뉴
+        addButton(buttonName: "address", width: nil, height: 40, top: 15, left: 70, right: -15, bottom: nil, target: mapView)   //주소 검색
+    }
+   
+    //사이드메뉴, 주소 찾기 버튼 추가
+    private func addButton(buttonName: String?, width: CGFloat?, height: CGFloat?, top: CGFloat?, left: CGFloat?, right: CGFloat?, bottom: CGFloat?, target: AnyObject) {
+        
+        if buttonName == "address" {
+            
+            mapView?.addSubview(addressView)
+            
+            addressView.setAttributes(buttonName: buttonName, width: width, height: height, top: top, left: left, right: right, bottom: bottom, target: target)
+            
+        } else {
+            let button = ShadowButton(type: .system)
+            
+            mapView?.addSubview(button)
+            
+            button.setAttributes(buttonName: buttonName, width: width, height: height, top: top, left: left, right: right, bottom: bottom, target: target)
+        }
+    }
+   /*
+    //충전 버튼, 예약 버튼 추가
+    private func addBottomButton(buttonName: String?, width: CGFloat?, height: CGFloat?, top: CGFloat?, left: CGFloat?, right: CGFloat?, bottom: CGFloat?, target: AnyObject, targetViewController: AnyObject) {
+        
+        self.view.addSubview(bottomButton)
+        
+        bottomButton.setAttributes(buttonName: "bottomButton", width: width, height: height, top: top, left: left, right: right, bottom: bottom, target: target, targetViewController: targetViewController)
+        
+        bottomButton.gone()
+        
+        self.view.sendSubviewToBack(bottomButton)
+    }
     
+    private func addSearchingConditionView(width: CGFloat?, height: CGFloat?, top: CGFloat?, left: CGFloat?, right: CGFloat?, bottom: CGFloat?, target: AnyObject) {
+        
+        mapView?.addSubview(searchingConditionView)
+        
+        searchingConditionView.setAttributes(width: width, height: height, top: top, left: left, right: right, bottom: bottom, target: target)
+    }
+    
+    private func addCurrentLocationButton(buttonName: String?, width: CGFloat?, height: CGFloat?, top: CGFloat?, left: CGFloat?, right: CGFloat?, bottom: CGFloat?, target: AnyObject) {
+        
+        let view = ShadowCircleView()
+        
+        mapView?.addSubview(view)
+        
+        view.setAttributes(buttonName: buttonName, width: width, height: height, top: top, left: left, right: right, bottom: bottom, target: target)
+    }
+    */
+    
+    //사이드 메뉴 버튼
+    @objc func menuButton(sender: UIButton!) {
+        print("MainViewController - menuButton tapped")
+        
+        self.performSegue(withIdentifier: "segueToLeftMenu", sender: self)
+    }
+    
+    //주소 찾기 버튼
+    @objc func addressButton(sender: UIButton!) {
+        print("MainViewController - addressButton tapped")
+        //self.performSegue(withIdentifier: "segueToAddress", sender: self)
+        
+    }
+    
+    //위치 권한 체크
     func hasLocationPermission() -> Bool {
         var hasPermission = false
         
@@ -59,36 +144,54 @@ class MainViewController: UIViewController, MTMapViewDelegate {
         return hasPermission
     }
     
-    func viewWillInitializeObjects(){
+    //Side Menu
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        requestGPSPermission()
-        //지도 설정
-        mTMapView = MTMapView(frame: mapView.bounds)
-        if let mTMapView = mTMapView {
-            
-            mTMapView.delegate = self
-            mTMapView.baseMapType = .standard
-            mapView.addSubview(mTMapView)
+        if segue.identifier == "segueToLeftMenu" {
+            if let sideMenuNavigationController = segue.destination as? SideMenuNavigationController {
+                
+                sideMenuNavigationController.settings = makeSettings()
+            }
         }
+        
+        /*if segue.identifier == "segueToAddress" {
+            if let searchingAddressViewController = segue.destination as? SearchingAddressViewController {
+                
+                if let userLatitude = locationManager.location?.coordinate.latitude , let userLongitude = locationManager.location?.coordinate.longitude{
+                
+                    searchingAddressViewController.userLatitude = userLatitude
+                    searchingAddressViewController.userLongitude = userLongitude
+                }
+                if let mapLatitude = mTMapView?.mapCenterPoint.mapPointGeo().latitude , let mapLongitude = mTMapView?.mapCenterPoint.mapPointGeo().longitude{
+                    
+                    searchingAddressViewController.mapLatitude = mapLatitude
+                    searchingAddressViewController.mapLongitude = mapLongitude
+                    
+                }
+
+                searchingAddressViewController.defaultAddress = (self.addressView.titleLabel?.text)!
+                searchingAddressViewController.delegate       = self
+            }
+        }*/
     }
     
-    //위치 권한
-    private func requestGPSPermission() {
-            
-        if !hasLocationPermission() {
-            let alertController = UIAlertController(title: "위치 권한이 요구됨", message: "내 위치 확인을 위해 권한이 필요합니다.", preferredStyle: UIAlertController.Style.alert)
-
-            let okAction = UIAlertAction(title: "Settings", style: .default, handler: {(cAlertAction) in
-                //Redirect to Settings app
-                UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
-            })
-
-            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel)
-            alertController.addAction(cancelAction)
-
-            alertController.addAction(okAction)
-
-            self.present(alertController, animated: true, completion: nil)
-        }
+    private func selectedPresentationStyle() -> SideMenuPresentationStyle {
+        
+        return .menuSlideIn
     }
+    
+    private func makeSettings() -> SideMenuSettings {
+        
+        let presentationStyle = selectedPresentationStyle()
+        presentationStyle.backgroundColor = .black
+        presentationStyle.presentingEndAlpha = 0.5
+        
+        var settings = SideMenuSettings()
+        settings.presentationStyle = presentationStyle
+        settings.menuWidth = self.view.frame.width * 0.85
+        
+        return settings
+    }
+    
+    
 }
