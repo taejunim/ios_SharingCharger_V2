3e9b93bb65035266b8da4393d3693a6110961001
guavaxt <guavaxt@gmail.com>
2021-04-16 16:16:37 +0900

    1. 즐겨찾기 화면 2. 포인트 이력 화면

---
+++ SharingCharger_V2/ViewController/MainStoryBoardViewController/Cell/FavoriteCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteCell.swift
+//  SharingCharger_V2
+//
+//  Created by guava on 2021/04/15.
+//
+
+class FavoriteCell: UITableViewCell {
+
+    @IBOutlet var chargerNameLabel: UILabel!
+    @IBOutlet var chargerAddressLabel: UILabel!
+    @IBOutlet var mapButton: UIButton!
+    @IBOutlet var deleteButton: UIButton!
+}
---
+++ SharingCharger_V2/ViewController/MainStoryBoardViewController/Cell/HistoryPointTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryPointTableCell.swift
+//  SharingCharger_V2
+//
+//  Created by guava on 2021/04/16.
+//
+
+class HistoryPointTableCell:UITableViewCell {
+    
+    @IBOutlet var pointDate: UILabel!
+    @IBOutlet var pointStateNm: UILabel!
+    @IBOutlet var usePoint: UILabel!
+    
+}
